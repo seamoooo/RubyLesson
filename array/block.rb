31adeec3f5216f8fa7da0e8989d96a69bc92691d
hmsyuma <hm.seamo@gmail.com>
2020-06-10 13:44:51 +0900

    test

--- array/block.rb
+++ array/block.rb
@@ -6,3 +6,21 @@ fruits.each_with_index{|fruit, i| puts "#{i}: #{fruit}" }
 kome.map.with_index{|fruit, i| puts "#{i}: #{fruit}" }
 
 #配列がブロック引数で渡される場合
+demensions = [
+  [10,20],
+  [30,40],
+  [50,60],
+]
+
+areas = []
+
+dimensions.each do |length,with|
+  areas << length * with
+end
+
+puts areas
+
+#配列の要素を受け取るブロック引数を()で囲むと、配列の要素を別々の引数として受け取ることができる
+demensions.each_with_index do |(index,width),i|
+  puts "length: #{index}, width: #{width}, i: #{i}"
+end
