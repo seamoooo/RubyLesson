05aa4a4330442fa6142b0878828f9c73b238f654
志茂秀真 <hm.seamo@gmail.com>
2021-09-07 09:27:59 +0900

    add lottery

---
+++ daily_resson/c_pro/lottery.rb
@@ -0,0 +1,16 @@
+# hit_number = gets.chomp.split(' ').map(&to_i)
+# number_count = gets.to_i
+# numbers = []
+# number_count.times { number << gets.chomp.split(' ').map(&to_i) }
+
+hit_number = [1, 2, 3, 4, 5,6]
+number_count = 3
+numbers = [[1, 5, 4, 2, 3, 6],[9,6, 2, 7, 1, 5],[32,9,87,33,41,60]]
+
+numbers.each do |item|
+  count = 0
+  item.each do |number|
+    count += 1 if hit_number.include?(number)
+  end
+  puts count
+end
