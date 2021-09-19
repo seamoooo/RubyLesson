6d13468175a7a0e4a325ebba510540227f48cd0b
志茂秀真 <hm.seamo@gmail.com>
2021-09-19 21:19:42 +0900

    create dre

---
+++ daily_resson/paiza/c_pro/log_filter.rb
@@ -0,0 +1,13 @@
+# N = gets.to_s
+# G = gets.sttip!
+# ary = readlines.map(&:to_i)
+
+
+# 文字列の数
+N = 3 
+# 抽出文字時列
+G = "paiza"
+logs = ["paizaonlinehackathon"]
+
+filter_logs = logs.select { |log| log.match?(/#{G}/) }
+filter_logs.empty? ? (puts "None") : (puts filter_logs)
