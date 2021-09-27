1768c6eee9d8dc85d8eade33da31f1e20c6759a0
志茂秀真 <hm.seamo@gmail.com>
2021-09-28 00:17:35 +0900

    class serch

--- daily_resson/paiza/object/serch_class.rb
+++ daily_resson/paiza/object/serch_class.rb
@@ -6,14 +6,21 @@ class Student
     @state = state
   end
 
+  def old
+   @old
+  end
+
+  def name
+   @name
+  end
 end
 
 student_conut = gets.to_i
 student_data = []
 student_conut.times { student_data << gets.chomp.split(' ').map(&:to_s)}
-one_year = get.to_i
+one_year = gets.to_i
 
 student_data.each do |val|
-  @student = Student.new(name: val[0], old:val[1], birth:val[2], state:val[3])
-  puts @student.name if @student.old.to_i == one_year
+  student = Student.new(name: val[0], old:val[1], birth:val[2], state:val[3])
+  puts student.name if student.old.to_i == one_year
 end
