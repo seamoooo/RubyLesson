0bd542b51fa1a19d7d7c11f26cf942869dc894c5
志茂秀真 <hm.seamo@gmail.com>
2021-10-15 11:55:48 +0900

    Alevel bord

--- daily_resson/paiza/a_lebel_up/tate.bord.rb
+++ daily_resson/paiza/a_lebel_up/tate.bord.rb
@@ -1,13 +1,13 @@
 h, w = gets.split(' ').map(&:to_i)
 
 border = []
-h.times { border << gets.split(' ')}
+h.times { border << gets.chomp.split('')}
 
 # eachはHashだけしか、key,valを取得できない
 border.each_with_index do |vals, y|
   vals.each_index do |x|
     if y == 0 || border[y - 1][x] == "#"
-      puts("#{y} #{x}") if y == (h - 1) || border[y - 1][x]
+      puts("#{y} #{x}") if y == (h - 1) || border[y + 1][x] == "#"
     end
   end
 end
