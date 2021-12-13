2f33ad065ae5ee9170157daa7b7a1d6e497fafc7
志茂秀真 <hm.seamo@gmail.com>
2021-12-13 17:44:32 +0900

    create todo app

---
+++ perfect_rails/todo/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # taskモデルのcrud操作を定義
+  resources :tasks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
