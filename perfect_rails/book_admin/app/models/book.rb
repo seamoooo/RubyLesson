7299066c48063a840f7bc0dc82a5cd3df8d5aa8a
志茂秀真 <hm.seamo@gmail.com>
2021-12-24 01:27:11 +0900

    add midle table

--- perfect_rails/book_admin/app/models/book.rb
+++ perfect_rails/book_admin/app/models/book.rb
@@ -4,4 +4,6 @@ class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000) }
 
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
