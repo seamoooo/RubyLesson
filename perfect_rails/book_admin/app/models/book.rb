f0244ecbe7f22588d460128937f487c21debd267
志茂秀真 <hm.seamo@gmail.com>
2021-12-24 23:01:07 +0900

    add enum

--- perfect_rails/book_admin/app/models/book.rb
+++ perfect_rails/book_admin/app/models/book.rb
@@ -6,4 +6,33 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  # add validates
+  validates :name, presence: true #dbの方でもnot nullを行っている前提
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+
+  # uniq validates
+  validate do |book|
+    if book.name.include?("programing")
+      book.errors[:name] << "this is programing book!!!!"
+    end
+  end
+
+  # hooks varidate
+  before_validation :lovely_cat
+
+  def lovely_cat
+    # gsub文字列置換  trueの場合、一致した値を渡す
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lobely #{matched}"
+    end
+  end
+
+  # rails側でenum型として扱う　フラグ管理
+  enum sales_status: {
+    resavetion: 0,
+    now_on_sale: 1,
+    end_of_print: 2,
+  }
 end
--- perfect_rails/book_admin/db/schema.rb
+++ perfect_rails/book_admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_092730) do
+ActiveRecord::Schema.define(version: 2021_12_24_132312) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 2021_12_23_092730) do
 
   create_table "book_authors", force: :cascade do |t|
     t.integer "book_id", null: false
-    t.integer "author_id", null: false
+    t.integer "auhor_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["auhor_id"], name: "index_book_authors_on_auhor_id"
     t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_092730) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "publisher_id", null: false
+    t.integer "sales_status"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
