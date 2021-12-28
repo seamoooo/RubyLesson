b4554c2da5c2b5257ab30c07dd1ac776ec87dc12
志茂秀真 <hm.seamo@gmail.com>
2021-12-28 18:31:53 +0900

    add destory

--- perfect_rails/book_admin/app/controllers/books_controller.rb
+++ perfect_rails/book_admin/app/controllers/books_controller.rb
@@ -1,9 +1,29 @@
 class BooksController < ApplicationController
+  protect_from_forgery expect: [:destroy]
+  # hooks
+  before_action :set_book, only: [:show, :destroy]
+
   def show
-    @book = Book.fund(params[:id]))
+    # pricate メソッドに以降
+    # @book = Book.fund(params[:id]))
 
     respond_to do |format|
       format.html
       format.json
     end
+
+  def destroy
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to "/" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
--- perfect_rails/todo/config/routes.rb
+++ perfect_rails/todo/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/book/:id" => "book#show"
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
 end
