39e1618ca6b264b72a4f93ab58c9459d873319f0
志茂秀真 <hm.seamo@gmail.com>
2021-12-29 12:01:46 +0900

    add routed

--- perfect_rails/book_admin/config/routes.rb
+++ perfect_rails/book_admin/config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+
+  resources :publishers
+  resource :prolile, only: %i{show edit update}
+
+  # 下記のようにリソースの親子関係を設定することができる
+  # resources :publishers do
+  #   resources :books
+  #   # 個別のリソースに対してのactionを定義することが可能
+  #   # publishers/:id/detail
+  #   member do
+  #     get 'detail'
+  #   end
+  #   # 全体のリソースに対するactionを設定できる
+  #   # publishers/serarch
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
 end
