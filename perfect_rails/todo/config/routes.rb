0b474273cd1f0432b4e020e0e308e43faf76b639
志茂秀真 <hm.seamo@gmail.com>
2022-01-11 10:32:40 +0900

    add cont

--- perfect_rails/todo/config/routes.rb
+++ perfect_rails/todo/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # taskモデルのcrud操作を定義
   resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "welcome#index"
 end
