6d902fadc248ca9cd32d6b8a24c79167f889cdeb
志茂秀真 <hm.seamo@gmail.com>
2022-01-17 23:54:29 +0900

    add from

---
+++ perfect_rails/projects/awesome_events/app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+
+  # アソシエーションが1対多の場合、関連付けのメソッド名.buildでmodelの関連付けを行う
+  def new
+    @event = current_user.created_event.build
+  end
+
+  def create
+    @event = current_user.created_event.build(event_params)
+
+    if @event.save
+      # redirect_to @event, notice: "作成しました。"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_at, :end_at
+    )
+  end
+end
--- perfect_rails/projects/awesome_events/app/models/user.rb
+++ perfect_rails/projects/awesome_events/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # userとeventは1対多　通常はmodel名のeventだが、関連元のユーザが作成したイベントであることを表現して、
+  # created_event
+  # https://qiita.com/tomoharutt/items/e548186c763079327ed1
+  has_many :created_event, class_name: "Event", foreign_key: "owner_id"
+
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
