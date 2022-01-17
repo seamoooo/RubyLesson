03fdc5e9b9599fa216e27b8130ab8b693a2859b5
志茂秀真 <hm.seamo@gmail.com>
2022-01-18 00:23:13 +0900

    add list page

--- perfect_rails/projects/awesome_events/app/controllers/events_controller.rb
+++ perfect_rails/projects/awesome_events/app/controllers/events_controller.rb
@@ -1,5 +1,9 @@
 class EventsController < ApplicationController
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   # アソシエーションが1対多の場合、関連付けのメソッド名.buildでmodelの関連付けを行う
   def new
     @event = current_user.created_event.build
@@ -9,7 +13,7 @@ class EventsController < ApplicationController
     @event = current_user.created_event.build(event_params)
 
     if @event.save
-      # redirect_to @event, notice: "作成しました。"
+      redirect_to @event, notice: "作成しました。"
     end
   end
 
--- perfect_rails/projects/awesome_events/app/controllers/welcome_controller.rb
+++ perfect_rails/projects/awesome_events/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
   def index
+    # ?は第2引数で指定した値が入れ替えられる
+    @events = Event.where("start_at < ?", Time.zone.now).order(:start_at)
   end
 end
