23683c969a5b95176bbbce8bdeeb5a47ff2e3a92
志茂秀真 <hm.seamo@gmail.com>
2022-01-20 19:15:17 +0900

    イベント参加処理とtypo修正

--- perfect_rails/projects/awesome_events/app/models/ticket.rb
+++ perfect_rails/projects/awesome_events/app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
   # nullを許可する
-  belongs_to :user, optinal: true
+  belongs_to :user, optional: true
   belongs_to :event
 
   validates :commnent, length: { maximum: 30}, allow_blank: true
