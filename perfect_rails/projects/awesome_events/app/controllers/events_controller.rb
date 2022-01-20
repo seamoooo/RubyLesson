e5f8707d6f45bc10f5c34c3ce7c4dd83b9eb7efc
志茂秀真 <hm.seamo@gmail.com>
2022-01-20 18:16:21 +0900

    削除と編集機能の追加

--- perfect_rails/projects/awesome_events/app/controllers/events_controller.rb
+++ perfect_rails/projects/awesome_events/app/controllers/events_controller.rb
@@ -4,17 +4,36 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  # アソシエーションが1対多の場合、関連付けのメソッド名.buildでmodelの関連付けを行う
-  def new
-    @event = current_user.created_event.build
-  end
-
   def create
     @event = current_user.created_event.build(event_params)
 
     if @event.save
       redirect_to @event, notice: "作成しました。"
     end
+  end
+
+  # アソシエーションが1対多の場合、関連付けのメソッド名.buildでmodelの関連付けを行う
+  def new
+    @event = current_user.created_event.build
+  end
+
+  def edit
+    # modelでリレーションを貼っているので、参照可能
+    @event = current_user.created_event.find(params[:id])
+  end
+
+  def update
+    # 作成したユーザーのみがアクセス可能なように設定
+    @event = current_user.created_event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました。"
+    end
+  end
+
+  def destroy
+    @event = current_user.created_event.find(params[:id])
+    @event.destroy!
+    redirect_to root_path, notice: "削除しました"
   end
 
   private
